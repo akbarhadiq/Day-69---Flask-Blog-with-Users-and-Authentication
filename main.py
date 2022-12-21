0354968ad04cee93672118c0fd341a72c471158e
akbarhadiq <akbarhadiq.ha@gmail.com>
2022-12-21 09:58:52 +0700

    add admin authentication

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, flash
+from flask import Flask, render_template, redirect, url_for, flash, abort
 from flask_bootstrap import Bootstrap
 from flask_ckeditor import CKEditor
 from datetime import date
@@ -8,6 +8,7 @@ from sqlalchemy.orm import relationship
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 from forms import CreatePostForm, RegisterForm, LoginForm
 from flask_gravatar import Gravatar
+from functools import wraps
 
 app = Flask(__name__)
 app.app_context().push()
@@ -43,6 +44,18 @@ class User(UserMixin,db.Model):
     name = db.Column(db.String(250), nullable=False)
 
 # db.create_all()  --> run this the first time you created a program, it will create the db for the program
+
+# admin only decorator --> i don't understand this. check the basics of decorators please
+def admin_only(function):
+
+    @wraps(function)
+    def decorated_function(*args, **kwargs):
+        if not current_user.is_authenticated or current_user.id != 1:
+            return abort(403)
+        else:
+            return function(*args, **kwargs)
+        
+    return decorated_function
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -148,6 +161,7 @@ def contact():
 
 
 @app.route("/new-post")
+@admin_only
 def add_new_post():
     form = CreatePostForm()
     if form.validate_on_submit():
@@ -166,6 +180,7 @@ def add_new_post():
 
 
 @app.route("/edit-post/<int:post_id>")
+@admin_only
 def edit_post(post_id):
     post = BlogPost.query.get(post_id)
     edit_form = CreatePostForm(
@@ -188,6 +203,7 @@ def edit_post(post_id):
 
 
 @app.route("/delete/<int:post_id>")
+@admin_only
 def delete_post(post_id):
     post_to_delete = BlogPost.query.get(post_id)
     db.session.delete(post_to_delete)
