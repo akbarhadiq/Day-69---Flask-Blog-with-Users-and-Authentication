77d1fd7894ae4167f240875c050850e4910a6838
akbarhadiq <akbarhadiq.ha@gmail.com>
2022-12-21 08:40:47 +0700

    first commit

---
+++ forms.py
@@ -0,0 +1,22 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField, EmailField
+from wtforms.validators import DataRequired, URL, Length
+from flask_ckeditor import CKEditorField
+
+##WTForm
+class CreatePostForm(FlaskForm):
+    title = StringField("Blog Post Title", validators=[DataRequired()])
+    subtitle = StringField("Subtitle", validators=[DataRequired()])
+    img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
+    body = CKEditorField("Blog Content", validators=[DataRequired()])
+    submit = SubmitField("Submit Post")
+
+class RegisterForm(FlaskForm):
+    email = EmailField("Email", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired(), Length(min=8, max=255, message="Password have to have a minimum of 8 characters")])
+    name = StringField("Name", validators=[DataRequired()])
+    submit = SubmitField("Register")
+
+class LoginForm(FlaskForm):
+    email = EmailField("Email", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
